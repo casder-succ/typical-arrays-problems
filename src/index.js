9a0a7a4c0f0d63e36d83c3a8d7aef277cd15edc6
casder-succ <casderiopus@gmail.com>
2021-08-22 19:03:04 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-
 exports.min = function min (array) {
-  return 0;
+  return !array ? 0 : array.length ? array.sort( (a, b) => a - b )[0] : 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+  return !array ? 0 : array.length ? array.sort( (a, b) => b - a )[0] : 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  return !array ? 0 : array.length ? array.reduce( (s, i) => s += i, 0 ) / array.length : 0;
 }
